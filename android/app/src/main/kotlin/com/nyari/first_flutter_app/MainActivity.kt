1ccf5f7eae338a9c72362434d48e443f9460dddd
Amanda Gonzales <amanderssgee@gmail.com>
2025-09-25 23:12:29 -0700

    Add regenerated iOS Flutter files (podhelper, xcconfigs) and Podfile; iOS platform 14

---
+++ android/app/src/main/kotlin/com/nyari/first_flutter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nyari.first_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,18 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps    // <-- add this
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
+@UIApplicationMain
+class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    GMSServices.provideAPIKey("AIzaSyBk5TUsxlsYpfzM16ljacudFH4NNx76Sks")   // <-- use your iOS key here
+    // If you use Places on iOS too:
+    // GMSPlacesClient.provideAPIKey("YOUR_IOS_MAPS_KEY")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
